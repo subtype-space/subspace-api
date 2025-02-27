b1b065bcd5e9cf0f2f1e4704ddab939cb0c4457f
asubowo <andrew.subowo@gmail.com>
2025-02-27 18:28:48 -0500

    updates

--- v1/controllers/trmnlcontroller.js
+++ v1/controllers/trmnlcontroller.js
@@ -1,10 +1,23 @@
 require('dotenv').config({ path: '.env' });
 
 
-module.exports = (request, response) => {
-    console.log('Accessed /status')
+module.exports = async (request, response) => {
+  console.log('Accessed /trmnl')
 
-    const version = request.query.apiversion;
-    response.json({message: `${version}`});
-    //response.json({message: "OK!", status: 200, api_version: `${ACTIVE_VERSION}`});
+  const { token, installation_callback_url } = request.query;
+  response.json({ message: `${token}, ${installation_callback_url}` });
+
+  const data = {
+    code: `${token}`,
+    client_id: process.env.TRMNL_CLIENT_ID,
+    client_secret: process.env.TRMNL_CLIENT_SECRET,
+    grant_type: "authorization_code",
+  };
+
+  trmnl_response = await fetch(process.env.TRMNL_ACCESS_TOKEN_URL, {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+
+  const access_token_data = await trmnl_response.json();
 }

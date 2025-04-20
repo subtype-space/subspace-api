c3f45552d629dd0d92d1ea0195ae72bfc1ee4b19
subtype <andrew.subowo@gmail.com>
2025-04-19 20:56:30 -0400

    update status controller

--- src/v1/controllers/statusController.ts
+++ src/v1/controllers/statusController.ts
@@ -6,7 +6,7 @@ const ACTIVE_VERSION = process.env.API_VERSION || "v1";
 
 const statusController = (request: Request, response: Response) => {
     console.log('Accessed /status')
-    response.json({message: "OK!", status: 200, api_version: `${ACTIVE_VERSION}`});
+    response.status(200).json({api_version: `${ACTIVE_VERSION}`})
 }
 
 export default statusController;

f06d8eca603b30c486e7b14633d3f0797f5b8bc9
asubowo <andrew.subowo@gmail.com>
2025-04-27 10:28:04 -0400

    JWT validation

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -7,7 +7,7 @@ export function checkAuth(req: Request, res: Response) {
   const expectedToken = process.env.SERVER_API_KEY
   logger.debug('Incoming authentication headers:', req.rawHeaders)
 
-  if (!authHeader || !authHeader.startsWith('Bearer ') || !expectedToken) {
+  if (!authHeader || !authHeader.startsWith('Bearer') || !expectedToken) {
     return false
   }
 

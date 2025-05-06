714f1379dbbe59bea501cb491d1c222b35ffb178
asubowo <andrew.subowo@gmail.com>
2025-05-06 17:08:05 -0400

    use express-jwt instead of manual

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,38 +1,31 @@
+import { expressjwt } from 'express-jwt'
 import { Request, Response, NextFunction } from 'express'
 import { logger } from './logger.js'
-import jwt, { Secret } from 'jsonwebtoken'
 
-export function validateJWT(req: Request) {
-  const secret = process.env.JWT_SECRET
-  if (!secret) {
-    logger.error('JWT secret is not defined in the environment!')
-    return null
-  }
-  const authHeader = req.headers.authorization
+const JWT_SECRET = process.env.JWT_SECRET
+
+export function logIncomingAuth(req: Request, res: Response, next: NextFunction) {
   logger.debug('Incoming headers:', req.headers)
+
+  const authHeader = req.headers.authorization
   if (!authHeader?.startsWith('Bearer ')) {
-    logger.error('No auth header!')
-    return null
+    logger.warn('No auth header or bad format')
+  } else {
+    const token = authHeader.split(' ')[1]
+    logger.debug('Inspecting token:', token)
   }
-  // Get token from auth header since it'd look like "Bearer <token>"
-  const token = authHeader.split(' ')[1]
-  logger.debug('Inspecting token', token)
 
-  try {
-    const decodedToken = jwt.verify(token, secret as Secret)
-    return decodedToken
-  } catch (error) {
-    logger.warn('JWT validation failed', error)
-    return null
-  }
-}
-
-export async function authRequired(req: Request, res: Response, next: NextFunction) {
-  const user = validateJWT(req)
-  if (!user) {
-    logger.warn('Incoming request failed JWT validation')
-    res.status(401).send({ message: 'Unauthorized' })
-  }
-  // handle user logic way way way later
   next()
 }
+
+export const authRequired = expressjwt({
+  secret: JWT_SECRET!,
+  algorithms: ['HS256'],
+  credentialsRequired: true,
+})
+
+export const authOptional = expressjwt({
+  secret: JWT_SECRET!,
+  algorithms: ['HS256'],
+  credentialsRequired: false,
+})
